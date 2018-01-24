1fe33d0918c8d3e80bfa7920d943b07405e35174
李威宏 Raikkonen Lee <raikkonen.lee@itts.com.tw>
2018-01-24 13:00:28 +0800

    Init ObserverPattern

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Subject : ISubject
+    {
+        public Observer Observer { get; private set; }
+
+        public Subject()
+        {
+
+        }
+
+        public void AddRegister()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRegister()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Observer : IObserver
+    {
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface ISubject
+    {
+        void AddRegister();
+        void RemoveRegister();
+        string GetStatus();
+        string SetStatus();
+    }
+
+    interface IObserver
+    {
+        void Update();
+    }
+}
